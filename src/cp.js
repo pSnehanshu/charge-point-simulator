689b6e305c32a8b4ff6b9863500dd7f04677b488
Snehanshu Phukon <snehanshuphukan@gmail.com>
2019-06-13 21:48:21 +0530

    Implementing uid upload

--- src/cp.js
+++ src/cp.js
@@ -1,31 +1,44 @@
 const express = require('express');
-const ChargePoint = require('./classes/chargepoint');
+const csv = require('csv-parse');
 const router = express.Router();
 module.exports = router;
 
-/* Structure of Charge point storage
-{
-    <serialNo>: <cpInstance>
-}
-*/
-
 router.get('/', async function (req, res) {
-    if (global.chargepoints[req.serialno]) {
-        var cp = global.chargepoints[req.serialno];
-    } else {
-        var cp = await ChargePoint(req.serialno);
-        global.chargepoints[req.serialno] = cp;
-    }
-    
-    res.render('cp', cp);
+    res.render('cp', req.cp);
 });
 
 router.post('/uid-upload', function (req, res) {
-    if (! req.files.uids) {
+    if (!req.files.uids) {
         return res.sendStatus(400);
     }
     var file = req.files.uids;
     var data = Buffer.from(file.data).toString();
 
-    res.send('<pre>' + data + '</pre>');
+    // Parse the csv
+    csv(data, {}, function (err, output) {
+        if (err) {
+            console.error(error);
+            return res.sendStatus(400);
+        }
+        var uids = transpose(output)[0];
+        req.cp.uids = arrayUnique(req.cp.uids.concat(uids));
+        res.redirect(`/cp/${req.cp.serialno}`);
+    });
 });
+
+// Source: https://stackoverflow.com/a/17428705/9990365
+function transpose(array = [[]]) {
+    return array[0].map((col, i) => array.map(row => row[i]));
+}
+// Source: https://stackoverflow.com/a/1584377/9990365
+function arrayUnique(array) {
+    var a = array.concat();
+    for (var i = 0; i < a.length; ++i) {
+        for (var j = i + 1; j < a.length; ++j) {
+            if (a[i] === a[j])
+                a.splice(j--, 1);
+        }
+    }
+
+    return a;
+}
