6f044b464144660a575a3a24167fa00e657a6c3a
Snehanshu Phukon <snehanshuphukan@gmail.com>
2019-06-20 20:36:09 +0530

    Showing appropriate unit time in sessionWorker

--- src/sessionWorker.js
+++ src/sessionWorker.js
@@ -7,9 +7,14 @@ var timer = null;
 
 process.on('message', function (msg) {
     if (msg.stopAfter) {
+        var stopAfter = msg.stopAfter + 'm';
+        if (msg.stopAfter > 60) {
+            stopAfter = Math.ceil(msg.stopAfter / 60) + 'h';
+        }
+
         if (msg.id) {
             id = msg.id;
-            process.send({ message: `Charging worker ${id} is running. Will stop after ${msg.stopAfter} min.` });
+            process.send({ message: `Charging worker ${id} is running. Will stop after ${stopAfter}.` });
         }
         stopAfter = msg.stopAfter * 60000;
         timer = startTimer(stopAfter, timer);
