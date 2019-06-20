944d46a362fa5b2551793dedfcc646ec65ea2114
Snehanshu Phukon <snehanshuphukan@gmail.com>
2019-06-20 21:26:59 +0530

    Handling reset command

--- src/handleCall.js
+++ src/handleCall.js
@@ -1,4 +1,21 @@
 module.exports = function (cp) {
-    
+    cp.on('Reset', function (msg, res) {
+        var payload = msg[3];
 
+        if (payload.type == 'Soft') {
+            // Soft reset
+        }
+        else if (payload.type == 'Hard') {
+            // Hard reset
+        } 
+        else {
+            // Send a CALLERROR
+            return res.error();
+        }
+
+        // TODO: Remove this later on
+        res.success({
+            status: 'Rejected'
+        });
+    });
 };
