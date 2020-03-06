af8f29c4eba1bcb7dff57d5649d14dde9614e5c4
Snehanshu Phukon <snehanshuphukan@gmail.com>
2020-03-06 16:25:10 +0530

    Implemented limited TriggerMessage

--- src/utils/handleCall.js
+++ src/utils/handleCall.js
@@ -7,7 +7,7 @@ module.exports = function (cp) {
         }
         else if (payload.type == 'Hard') {
             // Hard reset
-        } 
+        }
         else {
             // Send a CALLERROR
             return res.error();
@@ -31,5 +31,36 @@ module.exports = function (cp) {
         res.success({
             status: 'Rejected'
         });
+    });
+
+    cp.on('TriggerMessage', function (msg, res) {
+        // Trigger message is only supported in OCPP 1.6 and above.
+        if (cp.getParam('ocppVersion') != 'ocpp1.6') {
+            return;
+        }
+
+        let status = 'NotImplemented';
+        let { requestedMessage, connectorId } = msg[3];
+
+        switch (requestedMessage) {
+            case 'BootNotification':
+                status = 'Accepted';
+                cp.boot();
+                break;
+            case 'DiagnosticsStatusNotification': break;
+            case 'FirmwareStatusNotification': break;
+            case 'Heartbeat':
+                status = 'Accepted';
+                cp.send('Heartbeat');
+                break;
+            case 'MeterValues': break;
+            case 'StatusNotification':
+                status = 'Accepted';
+                cp.setStatus(cp.status, connectorId);
+                break;
+            default:
+        }
+
+        res.success({ status });
     });
 };
